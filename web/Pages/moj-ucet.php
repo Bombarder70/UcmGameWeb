587e69f00f041334039d12031c5bdda08dd82ba6
Bombarder70 <holespato@gmail.com>
2022-03-08 17:37:32 +0100

    rooms

--- web/Pages/moj-ucet.php
+++ web/Pages/moj-ucet.php
@@ -1,11 +1,13 @@
 <?php
 
-$idUser = 2; // TODO
-
 $profileView = new Component\ProfileView("users");
+$rooms = new \Component\TableLarge("rooms");
 
 $dia->template("
   <div class='mt-5'>
     {$profileView->show()}
+  </div>
+  <div class='container'>
+    ".$dia->card($rooms->show())."
   </div>
 ")->render();
