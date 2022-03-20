fea12bf9ee362666812c21305b559449fa910fdd
Bombarder70 <holespato@gmail.com>
2022-03-20 21:36:08 +0100

    update_score

--- web/Actions/update_score.php
+++ web/Actions/update_score.php
@@ -7,7 +7,38 @@
   $db->update(
     tableName: "players",
     rowId: 1,
-    data: $data
+    data: [
+      "score" => $data["score"]
+    ]
+  );
+
+  $response = $db->dbSelect(
+    "generator",
+    [
+      "where" => [
+        "id" => 1 // testovacie
+      ]
+    ]
+  );
+
+  $response = reset($response);
+
+  $otazky = (array)json_decode($response['structure']);
+
+  foreach ($otazky["quests"] as $key => $otazka) {
+    if ($otazka->zobrazena == false) {
+      $otazky["quests"][$key]->zobrazena = true;
+      $otazky["quests"][$key]->odpoved = (bool)$data["spravnost"];
+      break;
+    }
+  }
+
+  $db->update(
+    tableName: "generator",
+    rowId: 1,
+    data: [
+      "structure" => json_encode($otazky),
+    ]
   );
 
   echo 1;
