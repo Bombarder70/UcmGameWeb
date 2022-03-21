3a545bf840d1ac7fc12d4f56e06cac4e45980741
Bombarder70 <holespato@gmail.com>
2022-03-21 09:21:38 +0100

    get_quests

--- web/Actions/get_quests.php
+++ web/Actions/get_quests.php
@@ -2,15 +2,57 @@
 
   global $db;
 
+  $idGenerator = 1; // testovacie
+
   $response = $db->dbSelect(
     "generator",
     [
       "where" => [
-        "id" => 1 // testovacie
+        "id" => $idGenerator // testovacie
       ]
     ]
   );
 
   $response = reset($response);
 
-  echo $response['structure'];
+  // Vyber konkrtneho hraca
+  $players = $db->dbSelect(
+    "players",
+    [
+      "where" => [
+        "nickname" => \Core\Controllers\WebController::getParam("playerNickname")
+      ]
+    ]
+  );
+
+  $player = reset($players);
+
+  // Vyber struktury
+  $playersGenerators = $db->dbSelect(
+    "players_generators",
+    [
+      "where" => [
+        "id_player" => $player["id"],
+        "id_generator" => $idGenerator
+      ]
+    ]
+  );
+
+  // Ak hrac este nehral tuto variantu
+  if (empty($playersGenerators)) {
+    $db->insert_array([
+      'table' => "players_generators",
+      'table_data' => [
+        "id_player" => $player["id"],
+        "id_generator" => $idGenerator,
+        "structure" => $response['structure']
+      ]
+    ]);
+
+    $playerCurrentQuests = $db->getLastItem("players_generators");
+  } else {
+    $playerCurrentQuests = reset($playersGenerators);
+  }
+
+
+  echo $playerCurrentQuests['structure'];
