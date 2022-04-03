c1dfa567e856826d031d7da93cbc6e3b2aca77de
Bombarder70 <holespato@gmail.com>
2022-04-03 18:21:49 +0200

    get_score change

--- web/Actions/get_score.php
+++ web/Actions/get_score.php
@@ -2,15 +2,25 @@
 
   global $db;
 
-  $response = $db->dbSelect(
+  $lastPlayerId = $db->getLastItem("players")["id"];
+
+  $db->insert_array([
+    'table' => "players",
+    'table_data' => [
+      "nickname" => "Player_" . ($lastPlayerId + 1),
+      "score" => 50
+    ]
+  ]);
+
+  /*$response = $db->dbSelect(
     "players",
     [
       "where" => [
         "id" => 1 // Nas pirat2
       ]
     ]
-  );
+  );*/
 
-  $response = reset($response);
+  $playerData = $db->getLastItem("players");
 
-  echo $response['score'];
+  echo $playerData['score'];
