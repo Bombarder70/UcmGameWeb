a76219b29ef6fab61b25fa4224b82d9df81f618b
Bombarder70 <holespato@gmail.com>
2022-04-27 17:31:23 +0200

    get_generators changes

--- web/Actions/get_generators.php
+++ web/Actions/get_generators.php
@@ -2,12 +2,41 @@
 
 global $db;
 
-$generators = $db->dbSelect(
-  "generators",
+$uid = \Core\Controllers\WebController::getParam("uid");
+$status = "";
+$generators = [];
+
+$rooms = $db->dbSelect(
+  "rooms",
   [
-    "select" => "id, name"
+    "where" => [
+      "uid" => $uid
+    ]
   ]
 );
 
-echo json_encode($generators);
+if (!empty($rooms)) {
+  $room = reset($rooms);
+
+  $generators = $db->dbSelect(
+    "generator",
+    [
+      "select" => "id, name",
+      "where" => [
+        "id_room" => $room["id"]
+      ]
+    ]
+  );
+
+  if (empty($generators)) $status = "empty";
+  else $status = "success";
+} else {
+  $status = "error";
+}
+
+echo json_encode([
+  "status" => $status,
+  "data" => $generators
+]);
+
 
